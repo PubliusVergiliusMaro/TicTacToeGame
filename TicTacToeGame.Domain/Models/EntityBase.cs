9d2bc1a168a0f19a3ce362020a6e67016fe9c96b
Oleg <olegredko45@gmail.com>
2023-11-28 13:05:16 -0800

    Implement redirecting to the Game

--- TicTacToeGame.Domain/Models/EntityBase.cs
+++ TicTacToeGame.Domain/Models/EntityBase.cs
@@ -9,7 +9,7 @@ namespace TicTacToeGame.Domain.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; } // = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }  = DateTime.UtcNow;
         public Guid UniqueId { get; set; } = Guid.NewGuid();
         public bool IsDeleted { get; set; }
     }
--- TicTacToeGame.WebUI/Hubs/GameHub.cs
+++ TicTacToeGame.WebUI/Hubs/GameHub.cs
@@ -5,6 +5,10 @@ namespace TicTacToeGame.WebUI.Hubs
 {
     public class GameHub : Hub
     {
+        public async Task AcceptJoining(int roomId, Player player)
+        {
+            await Clients.All.SendAsync("AcceptJoining", roomId, player);
+        }
         public async Task JoinRoom(int roomId, Player player)
         {
             await Clients.All.SendAsync("JoinRoom", roomId, player);
