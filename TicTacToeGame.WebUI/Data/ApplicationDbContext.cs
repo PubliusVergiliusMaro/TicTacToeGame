b3d1838aaae23341ab9a5abad730865a81471d1d
Oleg <olegredko45@gmail.com>
2023-12-05 18:02:04 -0800

    Add Models to ApplicationDbContext

--- TicTacToeGame.WebUI/Data/ApplicationDbContext.cs
+++ TicTacToeGame.WebUI/Data/ApplicationDbContext.cs
@@ -1,10 +1,34 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 using TicTacToeGame.Domain.Models;
 
 namespace TicTacToeGame.WebUI.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<Player>(options)
     {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GamesHistory> GamesHistory { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<Game>()
+                .HasOne(g => g.Room)
+                .WithMany()
+                .HasForeignKey(g => g.RoomId);
+
+            builder.Entity<Game>()
+                .HasOne<GamesHistory>()
+                .WithMany(gh => gh.Games)
+                .HasForeignKey(g => g.GamesHistoryHostId);
+
+            builder.Entity<Game>()
+                .HasOne<GamesHistory>()
+                .WithMany(gh => gh.Games)
+                .HasForeignKey(g => g.GamesHistoryGuestId);
+        }
     }
 }
