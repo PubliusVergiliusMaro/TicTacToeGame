8515e955000327aa06233b6b1b50e2d04d639181
Artem <Artem@DESKTOP-M597TRG>
2023-12-19 14:59:05 +0200

    Fix chat

--- TicTacToeGame.Services/GameProcessService/ChatService.cs
+++ TicTacToeGame.Services/GameProcessService/ChatService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
 using TicTacToeGame.Domain.Models;
 using TicTacToeGame.Services.HubConnections;
 
@@ -6,13 +7,14 @@ namespace TicTacToeGame.Services.GameProcessService
 {
     public class GameChatService
     {
-        public List<KeyValuePair<string, string>> ChatMessages = new List<KeyValuePair<string, string>>();
+        public static List<KeyValuePair<string, string>> ChatMessages = new List<KeyValuePair<string, string>>();
         //private readonly GameHubConnection _gameHubConnection;
 
         private Game _currentGame;
         private Player _sender;
         private HubConnection _hubConnection;
         public bool NewMessage { get; set; } = false;
+        private Timer debounceTimer;
 
 
         public event Action UpdateComponent;
@@ -39,20 +41,28 @@ namespace TicTacToeGame.Services.GameProcessService
 
         public void AddMessage(string playerNickname, string message)
         {
-            ChatMessages.Add(new KeyValuePair<string, string>(playerNickname, message));
 
+            var newMessage = $"{playerNickname}: {message}";
+            // Check if the message is not already in the HashSet
             if (!_isChatVisible)
+            {
                 NewMessage = true;
-
+            }
             UpdateComponent?.Invoke();
+            
         }
         public async Task SendMessage(string message)
         {
-            NewMessage = false;
-            await _hubConnection.SendAsync("SendChatMessage", _currentGame.RoomId, _sender.UserName, message);
-            //await _gameHubConnection.SendChatMessage((int)_currentGame.RoomId, _sender.UserName, message);
-            UpdateComponent?.Invoke();
+            var newMessage = new KeyValuePair<string, string>(_sender.UserName, message);
 
+            // Add the new message to the list
+            ChatMessages.Add(newMessage);
+
+            // Send the chat message through SignalR
+            await _hubConnection.SendAsync("SendChatMessage", _currentGame.RoomId, _sender.UserName, message);
+
+            // Update the component
+            UpdateComponent?.Invoke();
         }
 
         public void ToggleChat()
@@ -64,5 +74,7 @@ namespace TicTacToeGame.Services.GameProcessService
 
             UpdateComponent?.Invoke();
         }
+       
+
     }
 }
